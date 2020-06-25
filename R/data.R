677069f651dcd47fad2a0f940fbcc3df8baaaa28
Akihiro Manabe <amanabe@affrc.go.jp>
2020-06-25 17:51:34 +0900

    :page_facing_up::pencil: Add license and data doc

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Length at age of an imaginary fish population
+#'
+#' A dataset of imaginary fish population with over 2000 individuals
+#'
+#' @format A data frame with 2143 rows and 2 variables:
+#' \describe{
+#'   \item{Length}{Length, in centimeter}
+#'   \item{Age}{Age, in year}
+#' }
+"sampledata"
