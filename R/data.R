9182de876362928c021d99061af6df242be7d4f1
Akihiro Manabe <amanabe@affrc.go.jp>
2020-08-24 17:55:51 +0900

    :large_blue_circle: Add freq_sample and al_sample

--- R/data.R
+++ R/data.R
@@ -8,3 +8,24 @@
 #'   \item{Age}{Age, in year}
 #' }
 "sampledata"
+
+#' Length frequency data of an imaginary fish population
+#'
+#' A data set of imaginary fish population with 2000 individuals
+#'
+#' @format A data frame with 2000 rows and 1 variables:
+#' \describe{
+#'   \item{Length}{Length, in centimeter}
+#' }
+"freq_sample"
+
+#' Length at age of an imaginary fish population
+#'
+#' A subsampled data of imaginary fish population with 400 individuals
+#'
+#' @format A data frame with 400 rows and 2 variables:
+#' \describe{
+#'   \item{Length}{Length, in centimeter}
+#'   \item{Age}{Age, in year}
+#' }
+"al_sample"
